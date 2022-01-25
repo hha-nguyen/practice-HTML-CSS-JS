1dd49f7f2fe4fac7a93af11be723451cb7a90aba
unknown <xamlol2982002@gmail.com>
2022-01-25 18:19:17 +0700

    the first praticing day

---
+++ day_1/script.js
@@ -0,0 +1,11 @@
+const heart = document.getElementsByClassName('card__heart');
+const heart_solid = document.createElement('i');
+heart_solid.className = "bx bxs-heart";
+// heart.addEventListener('click', () => {
+//     heart.replaceChild(heart.firstChild, heart_solid);
+// });
+
+console.log(heart);
+
+
+
