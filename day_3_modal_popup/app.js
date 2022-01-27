413e693a03e0651290287e003f3fb48df3f28db1
unknown <xamlol2982002@gmail.com>
2022-01-27 23:03:59 +0700

    complete day 3 practicing

--- day_3_modal_popup/app.js
+++ day_3_modal_popup/app.js
@@ -3,8 +3,15 @@ var modal = document.querySelector('.modal');
 var iconClose = document.querySelector('.modal__header i');
 var closeBtn = document.querySelector('#close');
 
-function toggleModal() {
+function toggleModal(e) {
     modal.classList.toggle('hide');
 }
 
 openBtn.addEventListener('click', toggleModal);
+iconClose.addEventListener('click', toggleModal);
+closeBtn.addEventListener('click', toggleModal);
+modal.addEventListener('click', (e) => {
+    if (e.target == e.currentTarget) {
+        toggleModal();
+    }
+});
